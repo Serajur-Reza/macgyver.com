960518d5f1f7435e6add4f1bde378791c4e89403
Serajur-Reza <37414920+Serajur-Reza@users.noreply.github.com>
2018-03-20 23:47:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#quote_gen").click(function(){
+	window.open("https://codepen.io/serajurreza/full/OOqzMM");
+});
+
+$("#wiki").click(function(){
+	window.open("https://codepen.io/serajurreza/full/VydjMR");
+});
+
+$("#weather").click(function(){
+	window.open("https://codepen.io/serajurreza/full/NyYxQd");
+});
